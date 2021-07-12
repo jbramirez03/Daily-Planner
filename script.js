30fd70fa3f7020581e4d4f86f121ad4c5e228dc8
temptado <jason1287712@gmail.com>
2021-07-11 22:25:38 -0400

    started local storage function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var currentDay = $("#currentDay");
 
-
+var am9 = $(".row-9am");
 
 
 
@@ -9,3 +9,32 @@ function displayDay () {
 }
 
 displayDay();
+
+
+function firstRow () {
+    var today = moment();
+    // console.log(today.format("hh A"));
+    ourTime = moment("9 am", "hh A");
+    console.log(ourTime);
+    if (today.isAfter(ourTime)) {
+        am9.children("textarea").addClass("past");
+    } else if (today === ourTime) {
+        am9.children("textarea").addClass("present");
+    }
+    
+    
+}
+ firstRow();
+
+
+ function savingItems (row) {
+    if (row.children("textarea").val() === "") {
+        alert("Planner must have event in order to be saved");
+        return;
+    }
+
+    var savedEvent = localStorage.getItem("events");
+
+
+ }
+ 

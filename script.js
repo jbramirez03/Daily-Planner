3c7a3371140c7cf507e777bd55b038e6c133926f
temptado <jason1287712@gmail.com>
2021-07-13 19:20:19 -0400

    completed first part of conditional for class change to textarea

--- script.js
+++ script.js
@@ -16,6 +16,15 @@ displayDay();
         individualHour = parseInt($(this).attr("id").split("hour")[1]);
 
         console.log(hourToday, individualHour);
+
+        if(hourToday > individualHour) {
+            $(this).children("textarea").addClass("past");
+            $(this).children("textarea").removeClass("present");
+            $(this).children("textarea").removeClass("future");
+
+
+        }
+
        })
    }
 
